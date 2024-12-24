35983d3c49528af54ed030a3aa990171a2f52edf
di351 <di35180@gmail.com>
2024-12-24 15:19:18 +0900

    feat: 기본 세팅

---
+++ YourMentor/YourMentor/YourMentorApp.swift
@@ -0,0 +1,17 @@
+//
+//  YourMentorApp.swift
+//  YourMentor
+//
+//  Created by 이다경 on 12/24/24.
+//
+
+import SwiftUI
+
+@main
+struct YourMentorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
